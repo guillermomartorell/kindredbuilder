28433edead700031e817c3ce4fcdece2025ad5a8
GUILLERMO MARTORELL <guillermo.martorell@GMAIL.com>
2021-03-24 22:59:25 -0300

    dark theme, attribute storing logic change

---
+++ src/components/Kindred/KindredAttributes/attributes.js
@@ -0,0 +1,48 @@
+export const str = [
+  "Strength",
+  "• You can easily crush a beer can. (20 kg: a Christmas tree, a stop sign",
+  "•• You are physically average. (45 kg: a toilet)",
+  "••• You might be able to break open a wooden door. (115 kg: a large human, an empty coffin, a refrigerator)",
+  "•••• You are a prime physical specimen, likely with very visible musculature. (180 kg: a full coffin, an empty dump- ster)",
+  "••••• You are a true powerhouse and can likely break open a metal fire door, tear open a chain-link fence, or snap open a chained gate. (250 kg: a motorcycle, a piano)",
+];
+export const dex = [
+  "Dexterity",
+  "• You can run, but balance and dodging are a challenge.",
+  "•• Your sprint is solid, and sometimes you appear graceful.",
+  "••• Your agility is impressive, and your coordination is as good as any trained amateur.",
+  "•••• You could excel at acrobatics and move in a way few humans can.",
+  "••••• Your movements are liquid and hypnotic – almost super- human.",
+];
+export const sta = [
+  "Stamina",
+  "• Even lesser exertions make you winded.",
+  "•• You can take a beating, but consider suing for peace",
+  "••• Several days of hard hiking with a backpack is no problem for you.",
+  "•••• You could win a marathon or take copious amounts of pain, at least physically.",
+  "••••• Even if you were a mortal, you’d never break a sweat.",
+];
+export const cha = [
+  "Charisma",
+  "• You can speak clearly, though few people tend to listen.",
+  "•• Generally likeable despite your undead nature, you may even have friends.",
+  "••• People trust you implicitly, and you easily make friends.",
+  "•••• You possess significant personal magnetism and draw followers like flies.",
+  "••••• You could lead a city in rebellion, if you so choose.",
+];
+export const man = [
+  "Manipulation",
+  "• As long as you stay honest, you can convince people to do what you want.",
+  "•• Your ability to deceive surpasses the will of the weak-willed and simple-minded.",
+  "••• You never have to pay full price for anything.",
+  "•••• You could win a marathon or take copious amounts of pain, at least physically.",
+  "••••• You could be a cult leader or a politician.",
+];
+export const com = [
+  "Composure",
+  "• The slightest insult or confrontation could drive you to frenzy.",
+  "•• You can subdue your predatory instincts in most non-hostile situations.",
+  "••• Others look to you for guidance when the blood spatter hits the fan.",
+  "•••• You can effortlessly bluff at cards and can manage your Beast to some extent.",
+  "••••• The Beast is your pet.",
+];
---
+++ src/components/Kindred/KindredAttributes/skills.js
@@ -0,0 +1,120 @@
+export const ath = [
+  "Athletics",
+  "• You were always attentive in gym class and you still have a spring in your step.",
+  "•• Despite being dead, you are still as fit as a mortal doing regular exercise.",
+  "••• You are fit as a fiddle and could play professional sports. Night games, at least.",
+  "•••• With your parkour abilities, why would you need to turn into a bat?",
+  "••••• Olympic records await you; only a very few peak humans can achieve what you can. Vampires mistake your skill for Physical Disciplines.",
+];
+export const bra = [
+  "Brawl",
+  "• You had a tough upbringing and had to fight to justify your place. You still have some moves.",
+  "•• You received some training in hitting someone hard and accurately.",
+  "••• You more than hold your own in a scrap.",
+  "•••• You either received Spetsnaz- quality training, or you have spent decades of afterlife in fights.",
+  "••••• You could win MMA championships even without the use of your vampiric powers.",
+];
+export const cra = [
+  "Stamina",
+  "• You are an amateur, but you know what you are doing.",
+  "•• Your craftsmanship is ad- mired for its functionality.",
+  "••• Your creations can be beauti- ful or horrifying, but their intent always clear.",
+  "•••• Your skill is highly respected among the kine and Kindred aware of it.",
+  "••••• You are often chosen to create the focal point for parties at Elysium.",
+];
+export const dri = [
+  "Drive",
+  "• You are a cautious driver, unlikely to make any mistakes.",
+  "•• You can put your foot down without much fear of an accident, providing visibility is good.",
+  "••• You have won car chases, earning a cool reputation among the Anarchs.",
+  "•••• You could be a stunt driver or the personal chauffeur of a Prince or Baron.",
+  "••••• You know cars inside and out. Few can match your skill and knowledge.",
+];
+export const fir = [
+  "Firearms",
+  "• You’ve fired a gun a few times, at the range or in less-formal circumstances.",
+  "•• You know (and know how) to keep your weapon clean, take it apart, and reassemble it.",
+  "••• You’ve been in the shit (“seen the elephant” if you’re older than a century) and come out the other side.",
+  "•••• You can handle trick shots, called shots, running shots – really anything ending in a gunshot.",
+  "••••• You’ve been practicing since the debut of the Winchester.",
+];
+export const lar = [
+  "Larceny",
+  "• You can pick a simple lock or a careless pocket.",
+  "•• You can hotwire a car or shoplift with ease.",
+  "••• You can identify the locations of security cameras and alarms so to best bypass them.",
+  "•••• You can defeat a keypad, re-tool an ID card, or crack a safe.",
+  "••••• You can get into – or out of – the vault of a mul- tinational bank.",
+];
+export const mel = [
+  "Melee",
+  "• You can swing a bat or blade and mostly hit the people you intend to.",
+  "•• Your clear competence with a weapon in hand should give attackers pause.",
+  "••• Your skill with a melee weapon is known throughout the domain.",
+  "•••• The fools brought a gun to your knife fight.",
+  "••••• You are the domain’s weap- onmaster, sought out by Kindred far and wide for your skill.",
+];
+export const ste = [
+  "Stealth",
+  "• Spotting you under the cover of darkness or in camouflage proves difficult.",
+  "•• You can sneak by casual observers and stalk unknowing victims without raising any hackles.",
+  "••• You evade patrolling guards, moving softly and hiding easily.",
+  "•••• Your subtle, silent passage could make you a worthy ninja – or a worthy foe for ninja.",
+  "••••• The Children of Haqim come to you for advice on stalking and hiding, if they can find you.",
+];
+export const sur = [
+  "Survival",
+  "• You know the trails and wilderness around your domain.",
+  "•• You spend more time outdoors than inside, and you can track anyone who doesn’t possess woodcraft.",
+  "••• You can subsist outside the city, setting traps for mortals and a shelter for yourself.",
+  "•••• You can thrive outside the city like the true predator you are.",
+  "••••• Gangrel run in packs with you – if they can keep up.",
+];
+export const ani = [
+  "Animal Ken",
+  "• Animals shy away from you but will not bolt or snap.",
+  "•• Animals are docile around you, acting as if you aren’t there, unless you build a rapport with them.",
+  "••• Animals treat you as if you were a warm, friendly owner, unless provoked to do other- wise.",
+  "•••• You draw animals into your orbit. Few attacking creatures maintain their aggression.",
+  "••••• You can sense an animal’s feelings and thoughts, and they can feel and act on yours.",
+];
+export const eti = [
+  "Etiquette",
+  "• You know how to address your local ruler without causing a faux pas.",
+  "•• You know the rules of etiquette in each of the domain’s popular nightspots.",
+  "••• You impress others with your command of politeness, deference, and grace.",
+  "•••• Your behavior sets trends, especially if you do something to buck the norm of etiquette.",
+  "••••• The Keeper of Elysium and the Harpies look to you to establish domain protocol.",
+];
+export const ins = [
+  "Insight",
+  "• You can see straight through hollow boasts.",
+  "•• You pick up on the hidden emotion between humans and even sometimes between Kindred.",
+  "••• You can provide psychotherapy even more effectively than someone who doesn’t want to eat their patient.",
+  "•••• You are an undead lie detector. Only the best con artists can slip one by you.",
+  "••••• People may be books of blood, but they are printed in very large type – easy to read.",
+];
+export const int = [
+  "Intimidation",
+  "• You can cut a scathing insult effectively.",
+  "•• You can push your way around most humans without difficulty.",
+  "••• Your swagger and your cruel behavior have earned you your reputation.",
+  "•••• You're far past mere physical threats.",
+  "••••• Even fellow Kindred may step back if you step up.",
+];
+export const lea = [
+  "Leadership",
+  "• You have led informal clubs before and can organize Kindred of like mind.",
+  "•• Your voice makes itself heard in council, and even your superiors sometimes listen.",
+  "••• You can command on the bat- tlefield and lead your soldiers into death.",
+  "•••• You can inspire the injured and dying to action, so long as you are there to lead them.",
+  "••••• Your words fill a vampire’s heart with enough strength to make it feel alive and beating.",
+];
+export const perf = [
+  "Performance",//continue here
+  "• You have led informal clubs before and can organize Kindred of like mind.",
+  "•• Your voice makes itself heard in council, and even your superiors sometimes listen.",
+  "••• You can command on the bat- tlefield and lead your soldiers into death.",
+  "•••• You can inspire the injured and dying to action, so long as you are there to lead them.",
+  "••••• Your words fill a vampire’s heart with enough strength to make it feel alive and beating.",
+];
